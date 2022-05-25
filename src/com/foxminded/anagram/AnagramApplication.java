ba5f673ec98156bb349c38caaf4b1f2181d2de6a
AndrewRosso <a.krasnov91@mail.ru>
2022-05-25 06:22:24 +0300

    update project

--- src/com/foxminded/anagram/AnagramApplication.java
+++ src/com/foxminded/anagram/AnagramApplication.java
@@ -1,17 +1,17 @@
 package com.foxminded.anagram;
-import java.util.Scanner;
 
 import com.foxminded.anagram.reversers.WordsReverser;
 
+import java.util.Scanner;
+
 public class AnagramApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	    Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         String inputText = scanner.nextLine();
-                
+
         WordsReverser myWords = new WordsReverser();
         System.out.println(myWords.reverseAllWords(inputText));
-	}
-
+    }
 }
---
+++ test/com/foxminded/anagram/WordsReverserTest.java
@@ -0,0 +1,59 @@
+package com.foxminded.anagram;
+
+import com.foxminded.anagram.reversers.WordsReverser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WordsReverserTest {
+
+    private final WordsReverser testInput = new WordsReverser();
+
+    @Test
+    void testReverseAllWordsWhenTheyAreNull() {
+        NullPointerException exception = assertThrows(NullPointerException.class,
+            () -> testInput.reverseAllWords(null));
+        assertEquals("Cannot invoke \"String.split(String)\" because \"input\" is null", exception.getMessage());
+    }
+
+    @Test
+    void testReverseAllWordsAsEmptyString() {
+        assertEquals("", testInput.reverseAllWords(""));
+    }
+
+    @Test
+    void testReverseAllWordsFromLettersOnly() {
+        assertEquals("olleH dlroW", testInput.reverseAllWords("Hello World"));
+    }
+
+    @Test
+    void testReverseAllWordsFromNumbersOnly() {
+        assertEquals("556 778", testInput.reverseAllWords("556 778"));
+    }
+
+    @Test
+    void testReverseAllWordsFromSpecialCharactersOnly() {
+        assertEquals("!@#$% ^&*><", testInput.reverseAllWords("!@#$% ^&*><"));
+    }
+
+    @Test
+    void testReverseAllWordsFromLettersAndNumbersOnly() {
+        assertEquals("olleH 123", testInput.reverseAllWords("Hello 123"));
+    }
+
+    @Test
+    void testReverseAllWordsFromLettersAndSpecialCharactersOnly() {
+        assertEquals("olleH naM!", testInput.reverseAllWords("Hello Man!"));
+    }
+
+    @Test
+    void testReverseAllWordsFromNumbersAndSpecialCharactersOnly() {
+        assertEquals("556! 778!", testInput.reverseAllWords("556! 778!"));
+    }
+
+    @Test
+    void testReverseAllWordsFromLettersAndNumbersAndSpecialCharactersOnly() {
+        assertEquals("olleH dlroW123!", testInput.reverseAllWords("Hello World123!"));
+    }
+}
